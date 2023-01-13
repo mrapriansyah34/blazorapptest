dcb016d59c0e95ab82bb25d3e23727e9f7434880
Mochamad Rizky Apriansyah <mr.apriansyah34@gmail.com>
2023-01-13 10:00:05 +0700

    Initial commit

---
+++ MyApp.Client/Auth/ServiceStackStateProvider.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Logging;
+using ServiceStack;
+using ServiceStack.Blazor;
+
+namespace MyApp.Client;
+
+/// <summary>
+/// Manages App Authentication State
+/// </summary>
+public class ServiceStackStateProvider : BlazorWasmAuthenticationStateProvider
+{
+    public ServiceStackStateProvider(BlazorWasmAuthContext context, ILogger<BlazorWasmAuthenticationStateProvider> log)
+        : base(context, log) { }
+}
